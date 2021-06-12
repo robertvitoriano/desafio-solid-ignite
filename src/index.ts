6f0763429a27fe1127845cffb50bb3802e38b12d
Robert Vitoriano <robertvitorianodev@gmail.com>
2021-06-12 08:10:59 -0300

    fix swagger route

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import { usersRoutes } from "./routes/users.routes";
 import swaggerUi from 'swagger-ui-express'
 import swaggerConfig from './swagger.json'
 const app = express();
-app.use('api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig))
 app.use(express.json());
 
+app.use('api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig))
 app.use("/users", usersRoutes);
 
 export { app };
