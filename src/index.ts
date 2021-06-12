334e7a53ffacc77df33f4ed4b1123455fe1890e9
Robert Vitoriano <robertvitorianodev@gmail.com>
2021-06-11 21:30:30 -0300

    add consfig swaggwe.jsonn

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
 import { usersRoutes } from "./routes/users.routes";
-
+import swaggerUi from 'swagger-ui-express'
+import swaggerConfig from './swagger.json'
 const app = express();
-
+app.use('api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig))
 app.use(express.json());
 
 app.use("/users", usersRoutes);
